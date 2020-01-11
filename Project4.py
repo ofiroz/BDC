f3b2a27a4b6288fdb41830e3805b71c7a05ece00
ofiroz <38746783+ofiroz@users.noreply.github.com>
2020-01-11 14:56:39 +0200

    Update Project4.py
    all redundant lines removed

--- Project4.py
+++ Project4.py
@@ -2,27 +2,31 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-frame_width = int( cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-frame_height =int( cap.get( cv2.CAP_PROP_FRAME_HEIGHT))
+# fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
 
-fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
-
-out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
+# out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280, 720))
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
-print(frame1.shape)
+# print(frame1.shape)
+
+
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5,5), 0)
-    _, thresh = cv2.threshold(blur, 5, 255, cv2.THRESH_BINARY) # thresh of 5 reconize breathing. 2 will reconize pulse!!
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    _, thresh = cv2.threshold(blur, 18, 255, cv2.THRESH_BINARY) # thresh of 5 reconize breathing. 2 will reconize pulse!!
+
+    # Breath recognition options
+    # 1. remove all big motions - dilate only breathing (only if tempo is correct)
+    # 2. focus on torso - dilate only torso
+
     dilated = cv2.dilate(thresh, None, iterations=3)
 
-    cv2.imshow("feed", dilated)
-
-
+    # cv2.imshow("feed", dilated)
 
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -35,8 +39,9 @@ while cap.isOpened():
         # cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 3)
     # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
-    image = cv2.resize(frame1, (1280,720))
-    out.write(image)
+    image = cv2.resize(frame1, (1280, 720))
+    # out.write(image)
+
     cv2.imshow("feed", frame1)
     # cv2.imshow("feed", diff)
     # cv2.imshow("feed", dilated)
@@ -50,4 +55,6 @@ while cap.isOpened():
 
 cv2.destroyAllWindows()
 cap.release()
-out.release()
+
+# out.release()
+
