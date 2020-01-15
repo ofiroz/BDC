7739c159138139aa981a4dc8ea64dc21a8779f09
ofiroz <38746783+ofiroz@users.noreply.github.com>
2020-01-15 08:53:19 +0200

    Update Project4.py

--- Project4.py
+++ Project4.py
@@ -2,15 +2,16 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
+if (cap is None):
+    print("Failed to read the image")
+    exit
+
+
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-# fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
-
-# out = cv2.VideoWriter("output.avi", fourcc, 5.0, (1280, 720))
-
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
+_, frame1 = cap.read()
+_, frame2 = cap.read()
 # print(frame1.shape)
 
 
@@ -18,7 +19,7 @@ while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    _, thresh = cv2.threshold(blur, 18, 255, cv2.THRESH_BINARY) # thresh of 5 reconize breathing. 2 will reconize pulse!!
+    _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY) # thresh of 5 reconize breathing. 2 will reconize pulse!!
 
     # Breath recognition options
     # 1. remove all big motions - dilate only breathing (only if tempo is correct)
@@ -36,11 +37,8 @@ while cap.isOpened():
         if cv2.contourArea(contour) < 900:
             continue
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 150, 255), 2)
-        # cv2.putText(frame1, "Status: {}".format('Movement'), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 3)
     # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
-    image = cv2.resize(frame1, (1280, 720))
-    # out.write(image)
 
     cv2.imshow("feed", frame1)
     # cv2.imshow("feed", diff)
@@ -48,7 +46,7 @@ while cap.isOpened():
 
 
     frame1 = frame2
-    ret, frame2 = cap.read()
+    _, frame2 = cap.read()
 
     if cv2.waitKey(40) == 27:
         break
@@ -56,5 +54,6 @@ while cap.isOpened():
 cv2.destroyAllWindows()
 cap.release()
 
-# out.release()
+
+
 
