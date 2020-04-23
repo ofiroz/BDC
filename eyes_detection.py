c94ceb3ea0c36fca789c0eeaa37ee031ffdd1ce5
Ofir Ozeri <38746783+ofiroz@users.noreply.github.com>
2020-04-23 15:35:49 +0300

    parameters fixed

--- eyes_detection.py
+++ eyes_detection.py
@@ -1,7 +1,19 @@
+'''
+must detect 1 or 2
+
+if found 2 eyes:
+1. calc ave x,y
+2. move N pixels from the ave point according the "Eyes : Stomach" ratio
+
+else if 1 eye found (side view):
+TODO
+
+'''
+
 import cv2
 
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-cap = cv2.VideoCapture(0)
+eye_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_eye.xml')
+cap = cv2.VideoCapture("videoSamples/WhatsApp Video 2020-04-23 at 13.45.40.mp4")
 if cap is None:
     print("Failed to read the image")
     exit
@@ -12,7 +24,7 @@ while True:
         break
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    eyes = eye_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=3, minSize=(20, 20), maxSize=(35, 35))
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 2)
 
@@ -23,3 +35,4 @@ while True:
         break
 cap.release()
 cv2.destroyAllWindows()
+
