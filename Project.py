d309739b6ac70392a9a5c1421c1c28f0d8851d33
Ofir Ozeri <38746783+ofiroz@users.noreply.github.com>
2020-05-19 21:05:13 +0300

    Threads added. now a thread runs every ~6 seconds and finds the person's center while the main (project) can show the live feed

--- Project.py
+++ Project.py
@@ -1,13 +1,22 @@
 import cv2
 import numpy as np
 import glob
+import person_center
+import background_thread
+# import importlib
+# from PIL import Image
+# from resizeimage import resizeimage
+from threading import Thread
 
+# IMPORT A CLASS OF IMPORTS.. LOOKS BETTER
 
 def readVideo(video_name):
-    capture = cv2.VideoCapture(video_name)  # 0 for camera OR "WhatsApp Video 2020-01-23 at 16.21.02.mp4"
+    capture = cv2.VideoCapture(0)  # 0 for camera OR video_name
     if capture is None:
         print("Failed to read the image")
         exit
+    # resized_capture = resizeimage.resize_thumbnail(capture, [640, 360])
+
     return capture
 
 
@@ -48,9 +57,20 @@ def video_processing(vid_name):
 
             if cv2.contourArea(contour) < 1000:
                 continue
-            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 150, 255), 2)
+            cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 150, 255), 2) # enlarge the image in the rectangle
+
+
         # cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
 
+        # **********************
+        '''
+        croped_movement = frame1[y:y + h, x:x + w]
+        croped_movement = cv2.dilate(croped_movement, None, iterations=3)
+        frame1 = frame1 + croped_movement
+        cv2.imshow("feed", frame1)
+        # cv2.waitKey(0)
+        '''
+        # **********************
         cv2.imshow("feed", frame1)
         # cv2.imshow("feed", diff)
         # cv2.imshow("feed", dilated)
@@ -84,4 +104,23 @@ def main_func():
 # 1. remove all big motions - dilate only breathing (only if tempo is correct)
 # 2. focus on torso - dilate only torso
 
-main_func()
+if __name__ == "__main__":
+    t = Thread(target=background_thread.call_background_thread)
+    keep_going = True
+    t.start()
+
+    main_func()
+
+    ''' # FROM PERSON_DETECTION_ML.PY #
+    
+    image_path = 'WhatsApp Image 2020-04-25 at 09.29.42.jpeg'
+
+    cv2.imshow("cropped", Person_detection_ML.Center_coordinates(image_path))
+    cv2.waitKey(0)
+    '''
+
+
+
+    # when the program ends kill thread
+    keep_going = False
+    t.join()
