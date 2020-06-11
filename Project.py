02f2842b77c5935a4fc8d585d809e8b277869816
Ofir Ozeri <38746783+ofiroz@users.noreply.github.com>
2020-06-11 15:45:34 +0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,21 +1,8 @@
-import cv2
-import numpy as np
-import glob
-import person_center
 import background_thread
-# import importlib
-# from PIL import Image
-# from resizeimage import resizeimage
 from threading import Thread
-from matplotlib import pyplot as plt
-
 import warnings
 warnings.filterwarnings("ignore")
 # import breath_dilation
-import queue
-
-
-# IMPORT A CLASS OF IMPORTS.. LOOKS BETTER
 
 
 if __name__ == "__main__":
@@ -25,17 +12,6 @@ if __name__ == "__main__":
 
     m.start()
     t.start()
-    #background_thread.call_background_thread()
-    # print("Main Process got to main_func")
-    #background_thread.main_func() # if no PERSON detected - return - handle it below
-
-    ''' # FROM PERSON_DETECTION_ML.PY #
-    
-    image_path = 'WhatsApp Image 2020-04-25 at 09.29.42.jpeg'
-
-    cv2.imshow("cropped", Person_detection_ML.Center_coordinates(image_path))
-    cv2.waitKey(0)
-    '''
 
     # when the program ends kill thread
     t.join()
