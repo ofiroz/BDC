0aae8fe793f4443ba00873c4386e8a9da227a7af
Ofir Ozeri <38746783+ofiroz@users.noreply.github.com>
2020-08-28 18:44:46 +0300

    class updated. + all btns activated

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import os
 # import Process_vid
 from Process_vid import FrameHelper
 from threading import Thread
@@ -14,25 +16,26 @@ class GUI:
 
         self.root = Tk()  # Tk obj
         self.root.bind('<Control-e>', lambda e: self.end_program())  # closes the program
+        self.root.protocol("WM_DELETE_WINDOW", self.end_program)  # ask when closing
         self.flag_end = True  # if "Ctrl + e" is pressed it will be False
 
         self.menubutton = Menubutton(self.root, text="Menu")
         self.menubutton.menu = Menu(self.menubutton)
         self.menubutton["menu"] = self.menubutton.menu
-        # TODO: write a regular menu without the checks + find what is IntVar() + add each menu option to a folder
-        self.var1 = IntVar()
-        self.var2 = IntVar()
-        self.var3 = IntVar()
 
-        self.menubutton.menu.add_checkbutton(label="open video folder", variable=self.var1)
-        self.menubutton.menu.add_checkbutton(label="open logs folder", variable=self.var2)
-        self.menubutton.menu.add_checkbutton(label="open cascades folder", variable=self.var3)
+        # shows the bad results with the opencv official frontal face Haar casc
+        self.menubutton.menu.add_command(label="Motivation", command=self.proc.motivation)
+
+        self.menubutton.menu.add_command(label="Open Cascade Folder", command=lambda: self.open_folder(self.casc_path))
+        self.menubutton.menu.add_command(label="Open Logs Folder", command=lambda: self.open_folder(self.logs_path))
+        self.menubutton.menu.add_command(label="Open Videos Folder", command=lambda: self.open_folder(self.vid_path))
+        self.menubutton.menu.add_command(label="Open Pictures Folder", command=lambda: self.open_folder(self.pics_path))
         self.menubutton.grid(row=0)
 
         # self.buttons_state = [0, 0, 0]  # if pressed = 1
         self.label = ttk.Label(self.root, text="Some Welcome Label")
         self.label.grid(row=1, column=0, columnspan=3)
-        self.label.config(font=('Ariel', 18, 'bold')) # font_name, size, extra (bold, underline..)
+        self.label.config(font=('Ariel', 18, 'bold'))  # font_name, size, extra (bold, underline..)
 
         # label = ttk.Label(root, text='Some Welcome Label', font=('Ariel', 18, 'bold'))
         # label.grid(row=0, column=0, columnspan=3)
@@ -49,37 +52,81 @@ class GUI:
         self.btn3.grid(row=2, column=2)
         self.btn3.config(command=self.pulse_monitor)
 
+        # the 'r' is a conversion to raw string (unicode error)
+        self.casc_path = r"C:\Users\ofirozer\PycharmProjects\untitled\1PROJECT\custom haar cascade\Cascades"
+        self.logs_path = r"C:\Users\ofirozer\PycharmProjects\untitled\1PROJECT\custom haar cascade\Logs Research"
+        self.vid_path = r"C:\Users\ofirozer\PycharmProjects\untitled\1PROJECT\custom haar cascade\Videos"
+        self.pics_path = r"C:\Users\ofirozer\PycharmProjects\untitled\1PROJECT\Project ReWriten\Pictures_Set"
+
+    @staticmethod
+    def open_folder(path):
+        p = os.path.realpath(path)
+        os.startfile(p)
+
     # This function disables the input button - there is no need to press it a second time
-    def disable(self, btn):
+    @staticmethod
+    def disable(btn):
         btn.config(state="disabled")
         print("Disabled the", btn['text'], "button.", sep=" ")
+
+    @staticmethod
+    def enable(btn):
+        btn.config(state="enable")
+        print("Enabled the", btn['text'], "button.", sep=" ")
 
     # runs samples videos and returns person with a circled center
     def breath_detection(self):
         self.disable(self.btn2)
-        # print("*** btn2 disabled ***")
+        # self.proc.state = 'baby detection'
+        print('self.proc.state',self.proc.state)
 
         # 2 threads for to 2 infinity loops
-        t1 = Thread(target=self.proc.call_frame_processor)  # process the sample frame
+        t1 = Thread(target=self.proc.call_frame_processor)  # process the sample frame TODO: useless
         t2 = Thread(target=self.proc.show_vid)  # Shows the video
-        # t1 = Thread(target=Process_vid.call_frame_processor)  # process the sample frame
-        # t2 = Thread(target=Process_vid.show_vid)  # Shows the video
-
         t2.start()
-        # cv2.waitKey(500)
         t1.start()
-
         # when the program ends kill threads
         # t1.join()
         # t2.join()
 
-    # runs samples pictures and returns person with a circled center
+    # opens a new window with 2 choices:
+    # 1. detect from pictures
+    # 2. detect from video
     def baby_detection(self):
-        print("baby_detection WAS NOT YET WRITEN")
+        self.disable(self.btn1)
+        # self.proc.state = 'baby detection'
+        # print('self.proc.state', self.proc.state)
+        newWindow = Toplevel(self.root)
+        newWindow.title("tk2")
+        Label(newWindow, text="Please choose the detection platform:").grid(row=1, column=0, columnspan=2)
+
+        #  the only good looking method to pass an arg AND commence two action at the same time
+        newWindow.protocol("WM_DELETE_WINDOW", lambda e=self.btn1: [newWindow.destroy(), self.enable(e)])
+        newWindow.bind('<Control-e>', lambda e: self.end_program())  # closes the program - needed here as well
+
+        b1 = ttk.Button(newWindow, text="PictureDS")
+        b1.grid(row=2, column=0)
+        b1.config(command=self.baby_detection_pics)
+
+        b2 = ttk.Button(newWindow, text="Video")
+        b2.grid(row=2, column=1)
+        b2.config(command=self.baby_detection_vid)
+
+    # Run the cascade on pictures dataset
+    def baby_detection_pics(self):
+        print("Commencing baby detection on the Pictures DataSet")
+        t1 = Thread(target=self.proc.detect_from_pics)
+        t1.start()
+
+    # Run the cascade on a video
+    def baby_detection_vid(self):
+        print("Commencing baby detection on a Video")
+        self.proc.state = 'baby detection'  # TODO: reuse of the video processing method (show_feed)
+        t2 = Thread(target=self.proc.show_vid)
+        t2.start()
 
     def pulse_monitor(self):
         self.disable(self.btn3)
-        # print("*** btn3 disabled ***")
         # TODO: solve the [ WARN:0]
         # TODO: cancel the camera 0 activation
         App = get_pulse.getPulseApp()
@@ -87,10 +134,12 @@ class GUI:
         t3 = Thread(target=App.MY_main_loop)
         t3.start()
 
-    # Ctrl+e will end the program
+    # Ctrl+e OR clicking [x] will end the program
     def end_program(self):
         print("FLAG updated to FALSE - closing program")
-        self.flag_end = False
+        self.flag_end = False  # will kill all running threads
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.destroy()
 
 
 if __name__ == "__main__":
